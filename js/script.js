8202dddcfc3170d5ffa528b2ad04d5d43fbe16c1
schaubjo <93454118+schaubjo@users.noreply.github.com>
2023-12-03 20:36:32 -0500

    Fix ddscription

--- js/script.js
+++ js/script.js
@@ -1,21 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Your existing JavaScript code
 
-    // Initialize ScrollMagic controller
     var controller = new ScrollMagic.Controller();
 
-    // Add ScrollMagic scenes for each card
+    // ScrollMagic for cards
     document.querySelectorAll('.card').forEach(function (card, index) {
         var scene = new ScrollMagic.Scene({
             triggerElement: card,
-            triggerHook: 0.8, // Adjust as needed
+            triggerHook: 0.8,
             reverse: false
         })
-            .setClassToggle(card, 'fade-in') // Add a CSS class for the fade-in effect
+            .setClassToggle(card, 'fade-in')
             .addTo(controller);
 
-        // Optionally, add indicators for debugging (remove in production)
-        // scene.addIndicators();
+
     });
 });
 
